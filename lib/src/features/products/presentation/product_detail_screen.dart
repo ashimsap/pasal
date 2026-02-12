faa1a7dbcea3c3798b88e78a15ed764bae99d3cc
Ashim <ashimsap@gmail.com>
2026-02-12 20:53:56 +0545

    feat: update product details UI

--- lib/src/features/products/presentation/product_detail_screen.dart
+++ lib/src/features/products/presentation/product_detail_screen.dart
@@ -243,7 +243,6 @@ class _ProductDetailScreenState extends ConsumerState<ProductDetailScreen> {
       );
     }
 
-
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Row(
